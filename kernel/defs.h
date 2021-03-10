5cc50f90398a5892926edf7bb35482acc7ee49a0
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-10 21:44:28 +0800

    hi

--- kernel/defs.h
+++ kernel/defs.h
@@ -1,10 +1,5 @@
 #include "types.h"
 
-// struct file;
-// struct context;
-// struct proc;
-// struct pipe;
-
 // panic.c
 void loop();
 void panic(char *);
@@ -12,25 +7,19 @@ void panic(char *);
 // sbi.c
 void console_putchar(int);
 int console_getchar();
-// void set_timer(uint64);
 void shutdown();
 
 // console.c
 void consoleinit(void);
 void consputc(int);
 
-// logger.c
+// printf.c
 void printf(char *, ...);
-// #include "logger.h"
-// void printfinit(void);
-// void panic(char*);
-// // trap.c
-void trapinit();
-// void usertrapret();
-// // void set_usertrap(void);
-// // void set_kerneltrap(void);
 
-// // // string.c
+// trap.c
+void trapinit();
+
+// string.c
 int memcmp(const void *, const void *, uint);
 void *memmove(void *, const void *, uint);
 void *memset(void *, int, uint);
@@ -42,67 +31,9 @@ char *strncpy(char *, const char *, int);
 // syscall.c
 void syscall();
 
-// // // swtch.S
-// // void swtch(struct context *, struct context *);
-
-// // // loader.c
+// batch.c
 void batchinit();
 int run_next_app();
-// // int get_id_by_name(char *name);
-// // void loader(int, void *);
 
-// // proc.c
-// // struct proc *curr_proc();
-// // void exit(int);
-// // void procinit(void);
-// // void scheduler(void) __attribute__((noreturn));
-// // void sched(void);
-// // void yield(void);
-// // int fork(void);
-// // int exec(char *);
-// // int wait(int, int *);
-// // struct proc *allocproc();
-// // int fdalloc(struct file *);
-
-// // kalloc.c
-// void *kalloc(void);
-// void kfree(void *);
-// void kinit(void);
-
-// // vm.c
-// void kvminit(void);
-// void kvmmap(pagetable_t, uint64, uint64, uint64, int);
-// int mappages(pagetable_t, uint64, uint64, uint64, int);
-// pagetable_t uvmcreate(void);
-// void uvminit(pagetable_t, uchar *, uint);
-// uint64 uvmalloc(pagetable_t, uint64, uint64);
-// uint64 uvmdealloc(pagetable_t, uint64, uint64);
-// int uvmcopy(pagetable_t, pagetable_t, uint64);
-// void uvmfree(pagetable_t, uint64);
-// void uvmunmap(pagetable_t, uint64, uint64, int);
-// uint64 walkaddr(pagetable_t, uint64);
-// uint64 useraddr(pagetable_t, uint64);
-// void debugwalk(pagetable_t, int);
-// int copyin(pagetable_t, char*, uint64, uint64);
-// int copyout(pagetable_t, uint64, char*, uint64);
-
-// // timer.c
-// uint64 get_cycle();
-// void timerinit();
-// void set_next_timer();
-// uint64 get_time_ms();
-
-// // pipe.c
-// int pipealloc(struct file *, struct file *);
-// void pipeclose(struct pipe *, int);
-// int piperead(struct pipe *, uint64, int);
-// int pipewrite(struct pipe *, uint64, int);
-
-// // file.c
-// void fileclose(struct file *);
-// struct file* filealloc();
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x) / sizeof((x)[0]))
-
-// #define MIN(a, b) (a < b ? a : b)
-// #define MAX(a, b) (a > b ? a : b)
--- kernel/trap.c
+++ kernel/trap.c
@@ -1,22 +1,46 @@
-#include "riscv.h"
 #include "defs.h"
 #include "trap.h"
+#include "riscv.h"
+// #include "proc.h"
+// #include "riscv.h"
+// #include "memory_layout.h"
 
 extern char trampoline[], uservec[], boot_stack_top[];
 extern void* userret(uint64);
 
-// set up to take exceptions and traps while in the kernel.
-void trapinit(void)
-{
+
+void trapinit(void) {
+    // intr_on();
+    // set_kerneltrap();
     w_stvec((uint64)uservec & ~0x3);
 }
+
+// void kerneltrap() {
+//     if((r_sstatus() & SSTATUS_SPP) == 0)
+//         panic("kerneltrap: not from supervisor mode");
+//     panic("trap from kernel\n");
+// }
+
+// // set up to take exceptions and traps while in the kernel.
+// void set_usertrap(void) {
+//     w_stvec(((uint64) TRAMPOLINE + (uservec - trampoline)) & ~0x3); // DIRECT
+// }
+
+// void set_kerneltrap(void) {
+//     w_stvec((uint64)kerneltrap & ~0x3); // DIRECT
+// }
+
+// void unknown_trap() {
+//     error("unknown trap: %p, stval = %p sepc = %p\n", r_scause(), r_stval(), r_sepc());
+//     exit(-1);
+// }
 
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
 //
-void usertrap(struct trapframe *trapframe)
-{
+void usertrap(struct trapframe *trapframe){
+    //here
     if((r_sstatus() & SSTATUS_SPP) != 0)
         panic("usertrap: not from user mode");
 
@@ -53,11 +77,9 @@ void usertrap(struct trapframe *trapframe)
     shutdown();
 }
 
-//
-// return to user space
-//
 void usertrapret(struct trapframe* trapframe, uint64 kstack)
 {
+    //here
     trapframe->kernel_satp = r_satp();         // kernel page table
     trapframe->kernel_sp = kstack + PGSIZE; // process's kernel stack
     trapframe->kernel_trap = (uint64)usertrap;
@@ -69,11 +91,15 @@ void usertrapret(struct trapframe* trapframe, uint64 kstack)
 
     // set S Previous Privilege mode to User.
     uint64 x = r_sstatus();
-    x &= ~SSTATUS_SPP; // clear SPP to 0 for user mode
-    x |= SSTATUS_SPIE; // enable interrupts in user mode
+    x &= ~SSTATUS_SPP;// clear SPP to 0 for user mode
+    x |= SSTATUS_SPIE;// enable interrupts in user mode
     w_sstatus(x);
 
     // tell trampoline.S the user page table to switch to.
-    // uint64 satp = MAKE_SATP(p->pagetable);
+    // uint64 satp = MAKE_SATP(curr_proc()->pagetable);
+    // trace("return to user at %p\n", trapframe->epc);
+    // uint64 fn = TRAMPOLINE + (userret - trampoline);
+    // ((void (*)(uint64,uint64))fn)(TRAPFRAME, satp);
     userret((uint64)trapframe);
+
 }
