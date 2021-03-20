27c82f35f532126234ee84c78ddb3bf58e089e0d
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-20 15:11:40 +0800

    hi

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -49,7 +49,7 @@ uint64 sys_setpriority(int code) {
 uint64 sys_get_time(TimeVal *ts,int tz){
     printf("im here");
     uint64 rtn = get_time(ts,0);
-    print("%d , %d",ts->sec, ts->usec);
+    printf("%d , %d",ts->sec, ts->usec);
     printf("here i am \n");
     return rtn;
 }
