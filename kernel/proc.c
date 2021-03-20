eac0b96e0e0b34ef25ab970a1845c7367a31772e
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-20 14:47:10 +0800

    hi

--- kernel/proc.c
+++ kernel/proc.c
@@ -119,6 +119,6 @@ uint64 set_priority(int code) {
 
 uint64 get_time(TimeVal* ts, int tz) {
     ts->sec = get_cycle()/12500000;
-    ts->usec = get_cycle()%12500000/12.5;
+    ts->usec = (get_cycle()%12500000)/12.5;
     return 0;
 }
