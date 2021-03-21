cbf7e1b75117f5ac76ca5e8d05e5fc35183b03ed
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-21 11:17:40 +0800

    hi

--- kernel/proc.c
+++ kernel/proc.c
@@ -78,6 +78,10 @@ scheduler(void)
         chosen->state = RUNNING;
         chosen->stride+=chosen->pass; //将对应的 stride 加上其对应的步长 pass
         current_proc = chosen;
+        if(current_proc->stride >= 500*(chosen->stride)){
+            exit(-1);
+            continue;
+        }
         swtch(&idle.context, &chosen->context);
     }
 }
