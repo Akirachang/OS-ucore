5a4a2765b1c3d60bf2d2aad593ca8b19b122318a
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-25 15:00:42 +0800

    hi

--- kernel/trap.c
+++ kernel/trap.c
@@ -126,6 +126,8 @@ void usertrapret() {
     uint64 satp = MAKE_SATP(curr_proc()->pagetable);
     printf("return to user\n");
     uint64 fn = TRAMPOLINE + (userret - trampoline);
+        printf("here1");
     ((void (*)(uint64,uint64))fn)(TRAPFRAME, satp);
+    printf("here2");
 
 }
