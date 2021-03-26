272a437a2dc7eadb7dd1a4afbecd9b0da6ba3085
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-26 16:08:58 +0800

    hi

--- user/src/ch7_file1.c
+++ user/src/ch7_file1.c
@@ -6,8 +6,9 @@
 
 /// 测试 fstat，输出　Test fstat OK! 就算正确。
 
-int main() {
-    char* fname = "fname1";
+int main()
+{
+    char *fname = "fname1";
     int fd = open(fname, O_CREATE | O_WRONLY);
     assert(fd > 0);
     Stat stat;
@@ -16,6 +17,8 @@ int main() {
     assert(stat.mode == FILE);
     assert(stat.nlink == 1);
     close(fd);
+    // unlink(fname);
+    // It's recommended to rebuild the disk image. This program will not clean the file "fname1".
     puts("Test fstat OK!");
     return 0;
 }
