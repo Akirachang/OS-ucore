a6b12cbb163fe9554028b0dba7c965195fcb94fe
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-27 18:31:56 +0800

    hi

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -115,6 +115,7 @@ void syscall() {
             printf("args1 is %p \n",args[1]);
             ret = sys_mmap(args[0],args[1],(int)args[2]);
             printf("im here yoyo$$$$$$$\n");
+            printf("ret is: %d",ret);
             break;
         default:
             ret = -1;
