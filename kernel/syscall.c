af8a5f4115fe8730501378b1726881c73f7f9aa8
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-27 22:17:47 +0800

    hi

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -79,15 +79,16 @@ uint64 sys_mmap(uint64 start, uint64 len, uint64 port){
         }    
     }
 
+    if(((port&0x8)!=0) || ((port&0x7)==0)){
+        return -1;
+    }
+
     port = port<<1;
     port = port | 0b00001;
     port = port | 0b10000;
     printf("port is %d\n", port);
 
-    if(((port&0x8)!=0) || ((port&0x7)==0)){
-        return -1;
-    }
-
+    
     int mmp = mappages(p->pagetable, start, len, physical_addr, port);
 
     if(mmp == 0){
