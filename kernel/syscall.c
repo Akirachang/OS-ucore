1b1fa5d59ceb18034bba7e0fc483c4567d3416aa
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-27 18:30:31 +0800

    hi

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -72,8 +72,10 @@ uint64 sys_mmap(uint64 start, uint64 len, int port){
     printf("aaaaaaaaa");
     printf("mmp is %d: ",mmp);
     printf("\n");
-    if(mmp == 0 && !copyout(p->pagetable,start,(char*)physical_addr,len))
+    if(mmp == 0 && !copyout(p->pagetable,start,(char*)physical_addr,len)){
+        printf("here?\n");
         return len;
+        }
     else
         return -1;
 }
