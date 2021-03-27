60d595cb2fe726627670aed66ccb4025d17eac85
常晟 <changs17@mails.tsinghua.edu.cn>
2021-03-27 18:26:04 +0800

    hi

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -68,6 +68,7 @@ uint64 sys_mmap(uint64 start, uint64 len, int port){
     uint64 physical_addr = useraddr(p->pagetable,start);
     printf("*************");
     int mmp = mappages(p->pagetable, start, len, physical_addr, port);
+    int a = copyout(p->pagetable,start,physical_addr,len);
     printf("aaaaaaaaa");
     printf("mmp is %d: ",mmp);
     printf("\n");
