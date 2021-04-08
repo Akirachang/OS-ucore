9a5550116a4770364ba6b22669f02f7a755e7ce1
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-08 19:27:10 +0800

    revert

--- user/lib/stdlib.c
+++ user/lib/stdlib.c
@@ -1,26 +1,9 @@
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#ifndef __STDLIB_H__
+#define __STDLIB_H__
 
-static uint64 seed;
+int rand(void);
+void srand(unsigned);
+void panic(char*);
+void assert(int, int);
 
-void srand(unsigned s) {
-    seed = s - 1;
-}
-
-int rand(void) {
-    seed = 6364136223846793005ULL * seed + 1;
-    return seed >> 33;
-}
-
-void panic(char* m) {
-    puts(m);
-    exit(-100);
-}
-
-void assert(int f, int code) {
-    if(!f) {
-        exit(code);
-    }
-}
+#endif //__STDLIB_H__
