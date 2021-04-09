b9362ca4de17a55c5e16dd43fccdc56e37590103
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-09 12:53:20 +0800

    hi

--- kernel/proc.c
+++ kernel/proc.c
@@ -245,9 +245,10 @@ wait(int pid, int* code)
         // Scan through table looking for exited children.
         havekids = 0;
         for(np = pool; np < &pool[NPROC]; np++){
-            // printf("%d\n",np->state != UNUSED);
-            // printf("%d\n",np->parent == p);
-            // printf("%d\n",(pid <= 0 || np->pid == pid));
+            printf("%d",np->state != UNUSED);
+            printf("%d",np->parent == p);
+            printf("%d",(pid <= 0 || np->pid == pid));
+            printf("\n");
 
             if(np->state != UNUSED && np->parent == p && (pid <= 0 || np->pid == pid)){
                 havekids = 1;
