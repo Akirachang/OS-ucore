922a25e8480c9abfe48b63c188f66f6797b8329a
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-10 22:16:49 +0800

    hi

--- kernel/proc.c
+++ kernel/proc.c
@@ -242,7 +242,6 @@ wait(int pid, int* code)
     struct proc *p = curr_proc();
     printf("current proc is %d\n",p->pid);
     //?
-    fork();
     for(;;){
         // Scan through table looking for exited children.
         havekids = 0;
