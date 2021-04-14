abdd09c29e42d9443cb289b15cb2016020d26fc6
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-14 16:34:12 +0800

    hi

--- kernel/proc.c
+++ kernel/proc.c
@@ -243,7 +243,7 @@ wait(int pid, int* code)
     struct proc *np;
     int havekids;
     struct proc *p = curr_proc();
-    printf("current proc is %d\n",p->pid);
+    // printf("current proc is %d\n",p->pid);
     for(;;){
         // Scan through table looking for exited children.
         havekids = 0;
@@ -254,19 +254,19 @@ wait(int pid, int* code)
             // printf("%d\n",np->parent == p);
             // printf("%d",(pid <= 0 || np->pid == pid));
             // printf("\n");
-            if(np->parent!=0)
-                printf("parent pid is %d\n", np->parent->pid);
-            if(np->pid!=0){
-                printf("np pid %d\n", np->pid);
-                printf("\n");
-            }
+            // if(np->parent!=0)
+            //     printf("parent pid is %d\n", np->parent->pid);
+            // if(np->pid!=0){
+            //     printf("np pid %d\n", np->pid);
+            //     printf("\n");
+            // }
             // printf("%d %d %d\n",np->state != UNUSED,np->parent == p,(pid <= 0 || np->pid == pid));
             if(np->state != UNUSED && np->parent == p && (pid <= 0 || np->pid == pid)){
                 havekids = 1;
-                printf("there is a kid\n");
+                // printf("there is a kid\n");
                 if(np->state == ZOMBIE){
                     // Found one.
-                    printf("FOUND A ZOMBIE! %d\n", np->pid);
+                    // printf("FOUND A ZOMBIE! %d\n", np->pid);
                     np->state = UNUSED;
                     pid = np->pid;
                     *code = np->exit_code;
