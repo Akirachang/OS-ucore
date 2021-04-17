cc039494d4946c2d8302547585a93a756d915b47
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-17 14:51:18 +0800

    hi

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,7 @@ scheduler(void)
     for(;;){
         int all_done = 1;
         for(p = pool; p < &pool[NPROC]; p++) {
+            print("in loop\n");
             if(p->state == RUNNABLE) {
                 all_done = 0;
                 p->state = RUNNING;
